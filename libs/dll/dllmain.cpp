821de3f39b0572f8d5b429dc437e10c80aabda03
TheSilvered <78680412+TheSilvered@users.noreply.github.com>
2023-01-03 18:59:28 +0100

    Re-organized project files

--- libs/dll/dllmain.cpp
+++ libs/dll/dllmain.cpp
@@ -1,18 +1,8 @@
 #include "framework.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE h_module,
+                      DWORD   reason_for_call,
+                      LPVOID  reserved)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
     return TRUE;
 }
-
--- libs/dll/framework.h
+++ libs/dll/framework.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef FRAMEWORK_H
+#define FRAMEWORK_H
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+
+#endif // !FRAMEWORK_H
