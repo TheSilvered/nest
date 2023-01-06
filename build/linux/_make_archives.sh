d5bbd1d0c7571a1b1b057c527506d712b21954fd
TheSilvered <78680412+TheSilvered@users.noreply.github.com>
2023-01-06 13:51:00 +0100

    Update _make_archives.sh

--- build/linux/_make_archives.sh
+++ build/linux/_make_archives.sh
@@ -1,4 +1,5 @@
 VERSION="0.9.2"
 
-tar -czf "nest-${VERSION}-x64-linux.tar.gz" linux_libs linux_release/x64 linux_install_x64.sh
-tar -czf "nest-${VERSION}-x86-linux.tar.gz" linux_libs linux_release/x86 linux_install_x86.sh
+mkdir -p linux_archives
+tar -czf "linux_archives/nest-${VERSION}-x64-linux.tar.gz" linux_libs linux_release/x64 linux_install_x64.sh
+tar -czf "linux_archives/nest-${VERSION}-x86-linux.tar.gz" linux_libs linux_release/x86 linux_install_x86.sh
