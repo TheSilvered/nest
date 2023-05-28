09ac797a24c85b51e6b83995aa6a782b869eb4bd
TheSilvered <taffarello.da@gmail.com>
2023-05-28 11:20:27 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -10,26 +10,26 @@ if [ "$#" -eq 1 ] && [ "$1" = "x86" ]; then
 fi
 
 # Upgrade packages
-sudo apt-get --trivial-only update
-sudo apt-get --trivial-only upgrade
+sudo apt-get --y update
+sudo apt-get --y upgrade
 
 # Install gcc
-sudo apt-get --trivial-only install build-essential gcc-multilib g++-multilib
+sudo apt-get --y install build-essential gcc-multilib g++-multilib
 # Install package config
-sudo apt-get --trivial-only install pkg-config
+sudo apt-get --y install pkg-config
 
 if [ $x64 = true ]; then
     # Install SDL2 x64
     sudo dpkg --add-architecture amd64
-    sudo apt-get --trivial-only update
-    sudo apt-get --trivial-only upgrade
-    sudo apt-get --trivial-only install libsdl2-dev libsdl2-ttf-dev
+    # sudo apt-get --y update
+    # sudo apt-get --y upgrade
+    sudo apt-get --y install libsdl2-dev libsdl2-ttf-dev
 fi
 
 if [ $x86 = true ]; then
     # Instal SDL2 x86
     sudo dpkg --add-architecture i386
-    sudo apt-get --trivial-only update
-    sudo apt-get --trivial-only upgrade
-    sudo apt-get --trivial-only install libsdl2-dev:i386 libsdl2-ttf-dev:i386
+    # sudo apt-get --y update
+    # sudo apt-get --y upgrade
+    sudo apt-get --y install libsdl2-dev:i386 libsdl2-ttf-dev:i386
 fi
