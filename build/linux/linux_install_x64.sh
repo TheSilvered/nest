33b131d2ab3d270b56f16af097a631ac65a7ae63
TheSilvered <taffarello.da@gmail.com>
2023-05-28 11:04:58 +0200

    pls work

--- build/linux/linux_install_x64.sh
+++ build/linux/linux_install_x64.sh
@@ -1,7 +1,7 @@
 src_path="./linux_release/x64";
 
 sudo mkdir -p "/usr/lib/nest"
-sudo cp "./linux_libs/std"* /usr/lib/nest
-sudo cp "${src_path}/std"* /usr/lib/nest
+sudo cp "./linux_libs/"* /usr/lib/nest
+sudo cp "${src_path}/"* /usr/lib/nest
 sudo cp "${src_path}/nest" /usr/bin
 sudo cp "${src_path}/libnest.so" /usr/lib
