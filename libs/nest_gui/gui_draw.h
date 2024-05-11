9b9c97a7d022ed20863c14c8aeda445561afbbe6
TheSilvered <taffarello.da@gmail.com>
2024-05-11 18:46:24 +0200

    Fixed names

--- libs/nest_gui/gui_draw.h
+++ libs/nest_gui/gui_draw.h
@@ -9,16 +9,16 @@ extern "C" {
 #endif // !__cplusplus
 
 void GUI_draw_rect(SDL_Renderer *renderer, SDL_Rect *rect);
-SDL_Texture *draw_round_rect(SDL_Renderer *renderer, SDL_Rect rect,
-                             int rtl, int rtr, int rbl, int rbr,
-                             u8 r, u8 g, u8 b, u8 a);
-SDL_Texture *draw_round_border_rect(SDL_Renderer *renderer, SDL_Rect rect,
-                                    int border_thickness,
-                                    int rtl, int rtr, int rbl, int rbr,
-                                    u8 r_i, u8 g_i, u8 b_i, u8 a_i,
-                                    u8 r_b, u8 g_b, u8 b_b, u8 a_b);
-void draw_texture(SDL_Renderer *renderer, int x, int y, SDL_Texture *texture,
-                  SDL_Rect *clip);
+SDL_Texture *GUI_draw_round_rect(SDL_Renderer *renderer, SDL_Rect rect,
+                                 int rtl, int rtr, int rbl, int rbr,
+                                 u8 r, u8 g, u8 b, u8 a);
+SDL_Texture *GUI_draw_round_border_rect(SDL_Renderer *renderer, SDL_Rect rect,
+                                        int border_thickness,
+                                        int rtl, int rtr, int rbl, int rbr,
+                                        u8 r_i, u8 g_i, u8 b_i, u8 a_i,
+                                        u8 r_b, u8 g_b, u8 b_b, u8 a_b);
+void GUI_draw_texture(SDL_Renderer *renderer, int x, int y,
+                      SDL_Texture *texture, SDL_Rect *clip);
 
 #ifdef __cplusplus
 }
